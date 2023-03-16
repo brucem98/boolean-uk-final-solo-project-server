90b30af4ba966c0b7c2d7e59486753153e96a390
Bruce M <84877032+brucem98@users.noreply.github.com>
2023-03-16 23:03:03 +0000

    Re-define selectors in puppeteer scraper

--- src/resources/price/controller.js
+++ src/resources/price/controller.js
@@ -6,27 +6,24 @@ const getIngredientPrice = (async (req, res) => {console.log("inside getIngredie
 console.log("req.query", req.query.ingredient)
 try {
     const shoppingUrl = `https://www.tesco.com/groceries/en-GB/search?query=${req.query.ingredient}`;
-    const browser = await puppeteer.launch({ args : [
-        '--no-sandbox',
-        '--disable-setuid-sandbox'
-      ]});
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
     await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36');
     await page.goto(shoppingUrl, {waitUntil: 'networkidle2'});
     let data = await page.evaluate(()=> {
         let results = []
-        let items = document.querySelectorAll('.product-tile-wrapper')
+        let items = document.querySelectorAll('.product-details--wrapper')
         
         items.forEach((item) => {
-            const priceEl = item.querySelector('.value')
-            const unitOfMeasureEl = item.querySelector('.weight')
+            const priceEl = item.querySelector('.beans-price__text');
+            const unitOfMeasureEl = item.querySelector('.beans-price__subtext');
             if (priceEl){
             results.push({
-                product: item.querySelector('.ui__StyledLink-sc-18aswmp-0').innerText,
-                price: parseFloat(priceEl.innerText),
-                unitOfMeasure: unitOfMeasureEl.innerText.replace('/',''),
-                link: item.querySelector('.ui__StyledLink-sc-18aswmp-0').href
+                product: item.querySelector('.beans-link__text').innerText,
+                price: parseFloat(priceEl.innerText.replace('£', '')),
+                unitOfMeasure: unitOfMeasureEl.innerText,
+                link: item.querySelector('.beans-link__anchor').href,
             })
         }
         })
