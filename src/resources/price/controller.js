1dfff80f6c163d5ef69602ba11313f70131fd5d5
Bruce Milliken <84877032+brucem98@users.noreply.github.com>
2023-09-08 12:43:52 +0100

    Update web scraper Tesco queries

--- src/resources/price/controller.js
+++ src/resources/price/controller.js
@@ -1,52 +1,47 @@
 const prisma = require("../../utils/database");
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
-
-const getIngredientPrice = (async (req, res) => {console.log("inside getIngredientPrice")
-console.log("req.query", req.query.ingredient)
-try {
+const getIngredientPrice = async (req, res) => {
+  console.log("inside getIngredientPrice");
+  console.log("req.query", req.query.ingredient);
+  try {
     const shoppingUrl = `https://www.tesco.com/groceries/en-GB/search?query=${req.query.ingredient}`;
-    const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
+    const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
     const page = await browser.newPage();
-    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36');
-    await page.goto(shoppingUrl, {waitUntil: 'networkidle2'});
-    let data = await page.evaluate(()=> {
-        let results = []
-        let items = document.querySelectorAll('.product-details--wrapper')
-        
-        items.forEach((item) => {
-            const priceEl = item.querySelector('.beans-price__text')
-            const unitOfMeasureEl = item.querySelector('.beans-price__subtext')
-            if (priceEl){
-            results.push({
-                product: item.querySelector('.beans-link__text').innerText,
-                price: parseFloat(priceEl.innerText.replace('£', '')),
-                unitOfMeasure: unitOfMeasureEl.innerText,
-                link: item.querySelector('.beans-link__anchor').href
-            })
+    await page.setUserAgent(
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36"
+    );
+    await page.goto(shoppingUrl, { waitUntil: "networkidle2" });
+    let data = await page.evaluate(() => {
+      let results = [];
+      let items = document.querySelectorAll(".product-details--wrapper");
+
+      items.forEach((item) => {
+        const priceEl = item.querySelector(".beans-price__text");
+        const unitOfMeasureEl = item.querySelector(".beans-price__subtext");
+        if (priceEl) {
+          results.push({
+            // product: item.querySelector(".ddsweb-link__text").innerText,
+            price: parseFloat(priceEl.innerText.replace("£", "")),
+            // unitOfMeasure: unitOfMeasureEl.innerText,
+            link: item.querySelector(".ddsweb-link__anchor").href,
+          });
         }
-        })
-        return results
-    })
+      });
+      return results;
+    });
 
-    console.log("data: ", data)
+    console.log("data: ", data);
 
-    res.json(data)
+    res.json(data);
     await browser.close();
   } catch (error) {
-      console.error({error})
+    console.error({ error });
 
-      res.status(500).json({ error: error.message })
+    res.status(500).json({ error: error.message });
   }
-
-
-})
-
-
-
+};
 
 module.exports = {
-    getIngredientPrice
-}
-
-
+  getIngredientPrice,
+};
